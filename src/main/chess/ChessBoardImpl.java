9e42eeeac34dc06424be0f7a2b96572aae2fba0c
krewdreele <keele.drew00@gmail.com>
2023-10-03 10:34:43 -0600

    Enhance(started working on ChessGameImpl)

--- src/main/chess/ChessBoardImpl.java
+++ src/main/chess/ChessBoardImpl.java
@@ -2,55 +2,55 @@ package chess;
 
 public class ChessBoardImpl implements ChessBoard {
 
-    private ChessPiece[][] board;
+    private ChessPiece[][] pieces;
 
     public ChessBoardImpl(){
-        board = new ChessPiece[8][8];
+        pieces = new ChessPiece[8][8];
     }
     @Override
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        board[position.getRow()-1][position.getColumn()-1] = piece;
+        pieces[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     @Override
     public ChessPiece getPiece(ChessPosition position) {
-        return board[position.getRow()-1][position.getColumn()-1];
+        return pieces[position.getRow()-1][position.getColumn()-1];
     }
 
     @Override
     public void resetBoard() {
-        board = new ChessPiece[8][8];
+        pieces = new ChessPiece[8][8];
         resetWhitePieces();
         resetBlackPieces();
     }
 
     private void resetWhitePieces(){
-        board[0][0] = new Rook(ChessGame.TeamColor.WHITE);
-        board[0][1] = new Knight(ChessGame.TeamColor.WHITE);
-        board[0][2] = new Bishop(ChessGame.TeamColor.WHITE);
-        board[0][3] = new Queen(ChessGame.TeamColor.WHITE);
-        board[0][4] = new King(ChessGame.TeamColor.WHITE);
-        board[0][5] = new Bishop(ChessGame.TeamColor.WHITE);
-        board[0][6] = new Knight(ChessGame.TeamColor.WHITE);
-        board[0][7] = new Rook(ChessGame.TeamColor.WHITE);
+        pieces[0][0] = new Rook(ChessGame.TeamColor.WHITE);
+        pieces[0][1] = new Knight(ChessGame.TeamColor.WHITE);
+        pieces[0][2] = new Bishop(ChessGame.TeamColor.WHITE);
+        pieces[0][3] = new Queen(ChessGame.TeamColor.WHITE);
+        pieces[0][4] = new King(ChessGame.TeamColor.WHITE);
+        pieces[0][5] = new Bishop(ChessGame.TeamColor.WHITE);
+        pieces[0][6] = new Knight(ChessGame.TeamColor.WHITE);
+        pieces[0][7] = new Rook(ChessGame.TeamColor.WHITE);
         //white pawns
         for(int i=0; i<8; i++){
-            board[1][i] = new Pawn(ChessGame.TeamColor.WHITE);
+            pieces[1][i] = new Pawn(ChessGame.TeamColor.WHITE);
         }
     }
 
     private void resetBlackPieces(){
-        board[7][0] = new Rook(ChessGame.TeamColor.BLACK);
-        board[7][1] = new Knight(ChessGame.TeamColor.BLACK);
-        board[7][2] = new Bishop(ChessGame.TeamColor.BLACK);
-        board[7][3] = new Queen(ChessGame.TeamColor.BLACK);
-        board[7][4] = new King(ChessGame.TeamColor.BLACK);
-        board[7][5] = new Bishop(ChessGame.TeamColor.BLACK);
-        board[7][6] = new Knight(ChessGame.TeamColor.BLACK);
-        board[7][7] = new Rook(ChessGame.TeamColor.BLACK);
+        pieces[7][0] = new Rook(ChessGame.TeamColor.BLACK);
+        pieces[7][1] = new Knight(ChessGame.TeamColor.BLACK);
+        pieces[7][2] = new Bishop(ChessGame.TeamColor.BLACK);
+        pieces[7][3] = new Queen(ChessGame.TeamColor.BLACK);
+        pieces[7][4] = new King(ChessGame.TeamColor.BLACK);
+        pieces[7][5] = new Bishop(ChessGame.TeamColor.BLACK);
+        pieces[7][6] = new Knight(ChessGame.TeamColor.BLACK);
+        pieces[7][7] = new Rook(ChessGame.TeamColor.BLACK);
         //black pawns
         for(int i=0; i<8; i++){
-            board[6][i] = new Pawn(ChessGame.TeamColor.BLACK);
+            pieces[6][i] = new Pawn(ChessGame.TeamColor.BLACK);
         }
     }
 }
--- src/main/chess/ChessGameImpl.java
+++ src/main/chess/ChessGameImpl.java
@@ -1,30 +1,107 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ChessGameImpl implements ChessGame{
+
+    private ChessBoard board;
+    private TeamColor turn;
+    public ChessGameImpl(){
+        turn = TeamColor.WHITE;
+    }
     @Override
     public TeamColor getTeamTurn() {
-        return null;
+        return turn;
     }
 
     @Override
     public void setTeamTurn(TeamColor team) {
-
+        turn = team;
     }
 
     @Override
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        return null;
+        ChessPiece piece = board.getPiece(startPosition);
+        if(piece == null){
+            return null;
+        }
+        ArrayList<ChessMove> moves = new ArrayList<>(piece.pieceMoves(board, startPosition));
+        for(ChessMove move : moves){
+            TeamColor color = board.getPiece(move.getStartPosition()).getTeamColor();
+            if(invalidMove(move, color)){
+                moves.remove(move);
+            }
+        }
+        return moves;
     }
 
     @Override
     public void makeMove(ChessMove move) throws InvalidMoveException {
+        TeamColor color = board.getPiece(move.getStartPosition()).getTeamColor();
+        if(color != turn){
+            throw new InvalidMoveException("It is not your turn dummy!");
+        }
+        else if(invalidMove(move, color)){
+            throw new InvalidMoveException("Move leaves King in check!");
+        }
+        else{
+            move(move, null);
+            changeTurns();
+        }
+    }
 
+    private boolean invalidMove(ChessMove move, TeamColor color) {
+        boolean invalid;
+        ChessPiece takenPiece = board.getPiece(move.getEndPosition());
+        ChessMove undo = new ChessMoveImpl(move.getEndPosition(), move.getStartPosition(), null);
+        move(move, null);
+        invalid = isInCheck(color);
+        move(undo, takenPiece);
+        return invalid;
+    }
+
+    private void move(ChessMove move, ChessPiece replace){
+        ChessPiece start = board.getPiece(move.getStartPosition());
+        board.addPiece(move.getEndPosition(), start);
+        board.addPiece(move.getStartPosition(), replace);
+    }
+
+    private void changeTurns(){
+        if(turn == TeamColor.WHITE){
+            turn = TeamColor.BLACK;
+        }
+        else if(turn == TeamColor.BLACK){
+            turn = TeamColor.WHITE;
+        }
     }
 
     @Override
     public boolean isInCheck(TeamColor teamColor) {
+        ArrayList<ChessMove> possibleMoves = new ArrayList<>();
+        ChessPosition kingPosition = null;
+        for(int i=1; i<=8; i++){
+            for(int j=1; j<=8; j++){
+                ChessPosition position = new ChessPositionImpl(i, j);
+                ChessPiece piece = board.getPiece(position);
+                if(piece == null){
+                    continue;
+                }
+                if(piece.getPieceType() == ChessPiece.PieceType.KING && piece.getTeamColor() ==  teamColor){
+                    kingPosition = position;
+                }
+                else if(piece.getTeamColor() != teamColor) {
+                    possibleMoves.addAll(piece.pieceMoves(board, position));
+                }
+            }
+        }
+
+        for(ChessMove move : possibleMoves){
+            if(move.getEndPosition() == kingPosition){
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -40,11 +117,11 @@ public class ChessGameImpl implements ChessGame{
 
     @Override
     public void setBoard(ChessBoard board) {
-
+        this.board = board;
     }
 
     @Override
     public ChessBoard getBoard() {
-        return null;
+        return board;
     }
 }
